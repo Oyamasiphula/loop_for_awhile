eda8f4dd77afec707208f546cb0d4741bf1edceb
oyamasiphula <oyama@projectcodex.co>
2016-11-21 23:50:38 +0200

    initial commit

---
+++ totalPhoneBill.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+var totalPhoneBill = function(stringRecord){
+  var outGoingCallsAndSmsLog = stringRecord.split(', ');
+  
+    for (var i = 0; i < array.length; i++) {
+      array[i]
+    }
+
+};
+totalPhoneBill('call, sms, call, sms, sms')
