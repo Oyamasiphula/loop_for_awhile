28686bd67c37a0ad94b49b277bf2f9dd35b94f77
oyamasiphula <oyama@projectcodex.co>
2016-11-23 23:55:27 +0200

    added function that calculates the phone total bill and a test

--- totalPhoneBill.js
+++ totalPhoneBill.js
@@ -1,11 +1,30 @@
 const assert = require('assert');
 
-var totalPhoneBill = function(stringRecord){
-  var outGoingCallsAndSmsLog = stringRecord.split(', ');
-  
-    for (var i = 0; i < array.length; i++) {
-      array[i]
-    }
+var totalPhoneBill = function(stringRecord) {
+    var outGoingCallsAndSmsLog = stringRecord.split(', ');
 
+    var callCost = 2.65;
+    var totalCallCost;
+    var totCalls = [];
+
+    var smsCost = 0.65;
+    var totalSmsCost;
+    var totSms = [];
+
+
+    for (var i = 0; i < outGoingCallsAndSmsLog.length; i++) {
+        if (outGoingCallsAndSmsLog[i] === 'call') {
+            totCalls.push(outGoingCallsAndSmsLog[i]);
+        }
+        if (outGoingCallsAndSmsLog[i] === 'sms') {
+            totSms.push(outGoingCallsAndSmsLog[i]);
+        }
+    }
+    totalCallCost = totCalls.length * callCost;
+    totalSmsCost = totSms.length * smsCost;
+
+    const totalBill = totalCallCost + totalSmsCost;
+    return totalBill;
 };
-totalPhoneBill('call, sms, call, sms, sms')
+
+assert.equal(totalPhoneBill('call, sms, call, sms, sms'), 7.25 )
